27ceb7bf65eeeeac0a21f6bcb6019eea513333c5
asari@udel.edu <asari@udel.edu>
2021-12-15 00:04:08 -0500

    String contains

--- src/Contains.java
+++ src/Contains.java
@@ -1,7 +1,23 @@
 public class Contains {
 
+    public static void parseContents(String s) {
+        System.out.println("Option 1");
+        for (char c : s.toCharArray()) {
+            System.out.println(c);
+        }
+
+
+        System.out.println("Option 2");
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            System.out.println(c);
+        }
+
+    }
+
     public static void main(String[] args) {
         String s = "Hello";
         System.out.println(s.contains("ll"));
+        parseContents(s);
     }
 }
