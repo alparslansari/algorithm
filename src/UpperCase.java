9419cc6862cb48566c185ee7e38beaf13c55b488
asari@udel.edu <asari@udel.edu>
2021-12-14 23:37:59 -0500

    isUpper / lower

---
+++ src/UpperCase.java
@@ -0,0 +1,19 @@
+public class UpperCase {
+
+    public static boolean isUpperCase(String s) {
+        return s.chars().allMatch(Character::isUpperCase);
+    }
+
+    public static boolean isLowerCase(String s) {
+        //return s.chars().noneMatch(Character::isUpperCase);
+        return s.chars().allMatch(Character::isLowerCase);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isUpperCase("hello"));
+        System.out.println(isUpperCase("HELLO"));
+
+        System.out.println(isLowerCase("hello"));
+        System.out.println(isLowerCase("HELLO"));
+    }
+}
