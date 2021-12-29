5998a5ca4bbb3b1702fc860273c62de16ae24f1e
asari@udel.edu <asari@udel.edu>
2021-12-29 16:51:47 -0500

    28. Implement strStr()

--- src/leetcode/problems/_28ImplementstrStr.java
+++ src/leetcode/problems/_28ImplementstrStr.java
@@ -1,2 +1,50 @@
-package leetcode.problems;public class _28ImplementstrStr {
+package leetcode.problems;
+
+/** 28. Implement strStr()
+ * Implement strStr().
+ * Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+ * Clarification:
+ * What should we return when needle is an empty string? This is a great question to ask during an interview.
+ * For the purpose of this problem, we will return 0 when needle is an empty string. This is consistent to C's strstr() and Java's indexOf().
+ *
+ * Example 1:
+ * Input: haystack = "hello", needle = "ll"
+ * Output: 2
+ *
+ * Example 2:
+ * Input: haystack = "aaaaa", needle = "bba"
+ * Output: -1
+ *
+ * Example 3:
+ * Input: haystack = "", needle = ""
+ * Output: 0
+ *
+ * Constraints:
+ *     0 <= haystack.length, needle.length <= 5 * 104
+ *     haystack and needle consist of only lower-case English characters.
+ */
+public class _28ImplementstrStr {
+    public int strStr(String haystack, String needle) {
+        if(haystack.length() < needle.length()) return -1;
+        if(needle.length() == 0) return 0;
+        int foundPosition = -1;
+        for(int i=0;i<haystack.length();i++)
+        {
+            if(haystack.charAt(i)==needle.charAt(0) && haystack.length() - i >= needle.length())
+            {
+                // loop through needle if matching return start position
+                boolean isMatched = true;
+                for(int j=1;j<needle.length();j++)
+                {
+                    if(haystack.charAt(i+j)!=needle.charAt(j)) {
+                        isMatched = false;
+                        break;
+                    }
+                }
+                if(isMatched) return i;
+            }
+        }
+
+        return foundPosition;
+    }
 }
--- src/leetcode/problems/_28ImplementstrStrTester.java
+++ src/leetcode/problems/_28ImplementstrStrTester.java
@@ -1,2 +1,30 @@
-package leetcode.problems;public class _28ImplementstrStrTester {
+package leetcode.problems;
+
+import leetcode.utilities.TesterHelper;
+
+public class _28ImplementstrStrTester {
+    public static void main(String[] args) {
+        _28ImplementstrStr tester = new _28ImplementstrStr();
+        TesterHelper helper = new TesterHelper();
+        String haystack = "hello";
+        String needle = "ll";
+        int expected = 2;
+        helper.check(expected,tester.strStr(haystack, needle));
+
+        haystack = "aaaaaa";
+        needle = "bba";
+        expected = -1;
+        helper.check(expected,tester.strStr(haystack, needle));
+
+        haystack = "";
+        needle = "";
+        expected = 0;
+        helper.check(expected,tester.strStr(haystack, needle));
+
+        haystack = "a";
+        needle = "a";
+        expected = 0;
+        helper.check(expected,tester.strStr(haystack, needle));
+
+    }
 }
