895b9f86e694b19304f6bfae358c1be05717de6d
asari@udel.edu <asari@udel.edu>
2021-12-29 16:52:12 -0500

    adding helper function for tester

--- src/leetcode/utilities/TesterHelper.java
+++ src/leetcode/utilities/TesterHelper.java
@@ -1,2 +1,17 @@
-package leetcode.utilities;public class TesterHelper {
+package leetcode.utilities;
+
+public class TesterHelper {
+
+    public boolean check(int expected, int output)
+    {
+        if(expected == output) {
+            System.out.println("Test1 is passed.");
+            return true;
+        } else
+        {
+            System.out.println("Test1 is failed.");
+            return false;
+        }
+
+    }
 }
