9c0a215fcb25a2b6b244742ea2abf5dd028d41e2
asari@udel.edu <asari@udel.edu>
2021-12-30 12:54:46 -0500

    344. Reverse String

--- src/leetcode/problems/_344ReverseString.java
+++ src/leetcode/problems/_344ReverseString.java
@@ -1,2 +1,50 @@
-package leetcode.problems;public class _344ReverseString {
+package leetcode.problems;
+
+/** 344. Reverse String
+ * Write a function that reverses a string. The input string is given as an array of characters s.
+ * You must do this by modifying the input array in-place with O(1) extra memory.
+ *
+ * Example 1:
+ * Input: s = ["h","e","l","l","o"]
+ * Output: ["o","l","l","e","h"]
+ *
+ * Example 2:
+ * Input: s = ["H","a","n","n","a","h"]
+ * Output: ["h","a","n","n","a","H"]
+ *
+ * Constraints:
+ *     1 <= s.length <= 105
+ *     s[i] is a printable ascii character.
+ */
+public class _344ReverseString {
+
+    public void reverseString(char[] s)
+    {
+        int start = 0;
+        int end = s.length-1;
+        char c;
+        while(start<=end)
+        {
+            c = s[start];
+            s[start] = s[end];
+            s[end] = c;
+            start++;
+            end--;
+        }
+    }
+
+    public void reverseStringRecursive(char[] s)
+    {
+        helper(s, 0, s.length-1);
+    }
+
+    private void helper(char[] s, int left, int right) {
+        if(left>=right) return; // Base condition
+        char tmp = s[left];
+        s[left++] = s[right];
+        s[right--] = tmp;
+        helper(s, left,right);
+    }
+
+
 }
--- src/leetcode/problems/_344ReverseStringTester.java
+++ src/leetcode/problems/_344ReverseStringTester.java
@@ -1,2 +1,26 @@
-package leetcode.problems;public class _344ReverseStringTester {
+package leetcode.problems;
+
+import leetcode.utilities.TesterHelper;
+
+public class _344ReverseStringTester {
+    public static void main(String[] args) {
+        _344ReverseString tester = new _344ReverseString();
+        TesterHelper helper = new TesterHelper();
+        char[] test1 = {'h','e','l','l','o'};
+        char[] expected1 = {'o','l','l','e','h'};
+        tester.reverseString(test1);
+        helper.check(expected1,test1);
+
+        char[] test2 = {'H','a','n','n','a','h'};
+        char[] expected2 = {'h','a','n','n','a','H'};
+        tester.reverseString(test2);
+        helper.check(expected2,test2);
+
+        char[] test3 = {'a'};
+        char[] expected3 = {'a'};
+        tester.reverseString(test3);
+        helper.check(expected3,test3);
+
+
+    }
 }
