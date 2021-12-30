9b6c988e7e391e43402e7bafa83738221fbc6e66
asari@udel.edu <asari@udel.edu>
2021-12-30 07:04:15 -0500

    improving TesterHelper

--- src/leetcode/utilities/TesterHelper.java
+++ src/leetcode/utilities/TesterHelper.java
@@ -2,16 +2,36 @@ package leetcode.utilities;
 
 public class TesterHelper {
 
+    public static final String PASSED = "Test is passed.";
+    public static final String FAILED = "Test is failed.";
+
     public boolean check(int expected, int output)
     {
         if(expected == output) {
-            System.out.println("Test1 is passed.");
+            print(PASSED,expected,output);
             return true;
         } else
         {
-            System.out.println("Test1 is failed.");
+            print(FAILED,expected,output);
             return false;
         }
 
+    }
+
+    public boolean check(String expected, String output)
+    {
+        if(expected.equals(output)) {
+            print(PASSED,expected,output);
+            return true;
+        } else
+        {
+            print(FAILED,expected,output);
+            return false;
+        }
+
+    }
+
+    private <T> void print(String msg, T expected, T output){
+        System.out.println(msg+" Expected="+expected+" Output="+output);
     }
 }
