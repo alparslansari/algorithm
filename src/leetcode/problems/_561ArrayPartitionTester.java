a60770e068787c6497e3aa22fa75670f88771859
asari@udel.edu <asari@udel.edu>
2021-12-30 13:23:13 -0500

    561. Array Partition I

---
+++ src/leetcode/problems/_561ArrayPartitionTester.java
@@ -0,0 +1,18 @@
+package leetcode.problems;
+
+import leetcode.utilities.TesterHelper;
+
+public class _561ArrayPartitionTester {
+    public static void main(String[] args) {
+        _561ArrayPartition tester = new _561ArrayPartition();
+        TesterHelper helper = new TesterHelper();
+        int[] nums1 = {1,4,3,2};
+        int expected = 4;
+        helper.check(expected, tester.arrayPairSum(nums1));
+
+        int[] nums2 = {6,2,6,5,1,2};
+        expected = 9;
+        helper.check(expected, tester.arrayPairSum(nums2));
+
+    }
+}
