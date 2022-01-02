4f0361ebdbbd06b73db58bd01cd445cd670b7fe4
asari@udel.edu <asari@udel.edu>
2022-01-02 15:08:29 -0500

    Array example

---
+++ src/leetcode/array/DVDPlayer.java
@@ -0,0 +1,10 @@
+package leetcode.array;
+
+import leetcode.array.models.DVD;
+
+public class DVDPlayer {
+    public static void main(String[] args) {
+        DVD[] dvdCollection = new DVD[15];
+        System.out.println("dvdCollection length = "+dvdCollection.length);
+    }
+}
