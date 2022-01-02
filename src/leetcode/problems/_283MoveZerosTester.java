52169ed2ab83d36427d6954019255f9be60f556c
asari@udel.edu <asari@udel.edu>
2022-01-02 14:49:04 -0500

    283. Move Zeroes

--- src/leetcode/problems/_283MoveZerosTester.java
+++ src/leetcode/problems/_283MoveZerosTester.java
@@ -1,4 +1,21 @@
 package leetcode.problems;
 
+import leetcode.utilities.TesterHelper;
+
 public class _283MoveZerosTester {
+    public static void main(String[] args) {
+        _283MoveZeros tester = new _283MoveZeros();
+        TesterHelper helper = new TesterHelper();
+        int[] nums1 = {0,1,0,3,12};
+        int[] expected = {1,3,12,0,0};
+        tester.moveZeroes(nums1);
+        helper.check(expected,nums1);
+
+
+        int[] nums2 = {0};
+        int[] expected2 = {0};
+        tester.moveZeroes(nums2);
+        helper.check(expected2,nums2);
+
+    }
 }
