3329a6f8a4b75a21043e96cd37afa7886f6bede0
asari@udel.edu <asari@udel.edu>
2022-01-06 23:33:20 -0500

    Singly Linked List

---
+++ src/leetcode/datastructures/singlylinkedlist/SinglyListNode.java
@@ -0,0 +1,9 @@
+package leetcode.datastructures.singlylinkedlist;
+
+public class SinglyListNode {
+    int val;
+    SinglyListNode next;
+    SinglyListNode(int ele) {
+        this.val = ele;
+    }
+}
