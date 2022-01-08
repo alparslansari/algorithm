d0c31392e7d2dabcf0adcda3e633ae6f1c762716
asari@udel.edu <asari@udel.edu>
2022-01-08 11:42:01 -0500

    Singly Linked List

--- src/leetcode/datastructures/singlylinkedlist/SinglyLinkedList.java
+++ src/leetcode/datastructures/singlylinkedlist/SinglyLinkedList.java
@@ -19,11 +19,8 @@ public class SinglyLinkedList<T> {
     public void addFirst(T ele)
     {
         SinglyListNode<T> node = new SinglyListNode<T>(ele);
-        if(this.head == null) this.head = node;
-        else {
-            node.next = head;
-            head = node;
-        }
+        node.next = head;
+        head = node;
     }
 
     public void addLast(T ele)
@@ -63,5 +60,75 @@ public class SinglyLinkedList<T> {
         if (curr == null) return null;
         return (T) curr.val;
     }
+
+    /** Helper function to return the index-th node in the linked list. */
+    private SinglyListNode getNode(int index) {
+        SinglyListNode cur = head;
+        for (int i = 0; i < index && cur != null; ++i) {
+            cur = cur.next;
+        }
+        return cur;
+    }
+    /** Helper function to return the last node in the linked list. */
+    private SinglyListNode getTail() {
+        SinglyListNode cur = head;
+        while (cur != null && cur.next != null) {
+            cur = cur.next;
+        }
+        return cur;
+    }
+
+
+    /** Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list. */
+    public void addAtHead(int val) {
+        SinglyListNode cur = new SinglyListNode(val);
+        cur.next = head;
+        head = cur;
+        return;
+    }
+
+    /** Append a node of value val to the last element of the linked list. */
+    public void addAtTail(int val) {
+        if (head == null) {
+            addAtHead(val);
+            return;
+        }
+        SinglyListNode prev = getTail();
+        SinglyListNode cur = new SinglyListNode(val);
+        prev.next = cur;
+    }
+
+    /** Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. */
+    public void addAtIndex(int index, int val) {
+        if (index == 0) {
+            addAtHead(val);
+            return;
+        }
+        SinglyListNode prev = getNode(index - 1);
+        if (prev == null) {
+            return;
+        }
+        SinglyListNode cur = new SinglyListNode(val);
+        SinglyListNode next = prev.next;
+        cur.next = next;
+        prev.next = cur;
+    }
+
+    /** Delete the index-th node in the linked list, if the index is valid. */
+    public void deleteAtIndex(int index) {
+        SinglyListNode cur = getNode(index);
+        if (cur == null) {
+            return;
+        }
+        SinglyListNode next = cur.next;
+        if (index == 0) {
+            // modify head when deleting the first node.
+            head = next;
+        } else {
+            SinglyListNode prev = getNode(index - 1);
+            prev.next = next;
+        }
+    }
+
 
 }
