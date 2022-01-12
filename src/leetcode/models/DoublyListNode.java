a5d338e3f051f47b2240778c7988d15dbbdf4efb
asari@udel.edu <asari@udel.edu>
2022-01-11 22:39:58 -0500

    707. Design Linked List

---
+++ src/leetcode/models/DoublyListNode.java
@@ -0,0 +1,8 @@
+package leetcode.models;
+
+// Definition for doubly-linked list.
+public class DoublyListNode {
+    int val;
+    DoublyListNode next, prev;
+    DoublyListNode(int x) {val = x;}
+}
