7ee6dd769ccda8c936a9b84974cd6940499cb729
asari@udel.edu <asari@udel.edu>
2022-01-12 00:00:26 -0500

    708. Insert into a Sorted Circular Linked List

--- src/leetcode/models/Node.java
+++ src/leetcode/models/Node.java
@@ -8,6 +8,15 @@ public class Node {
 
     public Node() {}
 
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _next) {
+        val = _val;
+        next = _next;
+    }
+
     public Node(int _val,Node _prev,Node _next,Node _child) {
         val = _val;
         prev = _prev;
