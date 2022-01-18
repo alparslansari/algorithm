67d97ee30a3a2673c391008ddf614304c0151945
asari@udel.edu <asari@udel.edu>
2022-01-17 22:29:57 -0500

    599. Minimum Index Sum of Two Lists

---
+++ src/leetcode/problems/_599MinimumIndexSumOfTwoLists.java
@@ -0,0 +1,83 @@
+package leetcode.problems;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/** 599. Minimum Index Sum of Two Lists
+ * Suppose Andy and Doris want to choose a restaurant for dinner, and they both have a list of favorite restaurants represented by strings.
+ * You need to help them find out their common interest with the least list index sum. If there is a choice tie between answers, output all of them with no order requirement. You could assume there always exists an answer.
+ *
+ * Example 1:
+ * Input: list1 = ["Shogun","Tapioca Express","Burger King","KFC"], list2 = ["Piatti","The Grill at Torrey Pines","Hungry Hunter Steakhouse","Shogun"]
+ * Output: ["Shogun"]
+ * Explanation: The only restaurant they both like is "Shogun".
+ *
+ * Example 2:
+ * Input: list1 = ["Shogun","Tapioca Express","Burger King","KFC"], list2 = ["KFC","Shogun","Burger King"]
+ * Output: ["Shogun"]
+ * Explanation: The restaurant they both like and have the least index sum is "Shogun" with index sum 1 (0+1).
+ *
+ * Constraints:
+ *     1 <= list1.length, list2.length <= 1000
+ *     1 <= list1[i].length, list2[i].length <= 30
+ *     list1[i] and list2[i] consist of spaces ' ' and English letters.
+ *     All the stings of list1 are unique.
+ *     All the stings of list2 are unique.
+ */
+public class _599MinimumIndexSumOfTwoLists {
+
+    // Approach #1 Using HashMap
+    public String[] findRestaurant(String[] list1, String[] list2) {
+        HashMap < Integer, List< String >> map = new HashMap< >();
+        for (int i = 0; i < list1.length; i++) {
+            for (int j = 0; j < list2.length; j++) {
+                if (list1[i].equals(list2[j])) {
+                    if (!map.containsKey(i + j))
+                        map.put(i + j, new ArrayList< String >());
+                    map.get(i + j).add(list1[i]);
+                }
+            }
+        }
+        int min_index_sum = Integer.MAX_VALUE;
+        for (int key: map.keySet())
+            min_index_sum = Math.min(min_index_sum, key);
+        String[] res = new String[map.get(min_index_sum).size()];
+        return map.get(min_index_sum).toArray(res);
+    }
+
+    // Approach #2 Without Using HashMap
+    public String[] findRestaurantV2(String[] list1, String[] list2) {
+        List < String > res = new ArrayList < > ();
+        for (int sum = 0; sum < list1.length + list2.length - 1; sum++) {
+            for (int i = 0; i <= sum; i++) {
+                if (i < list1.length && sum - i < list2.length && list1[i].equals(list2[sum - i]))
+                    res.add(list1[i]);
+            }
+            if (res.size() > 0)
+                break;
+        }
+        return res.toArray(new String[res.size()]);
+    }
+
+    // Approach #3 Using HashMap (linear)
+    public String[] findRestaurantV3(String[] list1, String[] list2) {
+        HashMap < String, Integer > map = new HashMap < String, Integer > ();
+        for (int i = 0; i < list1.length; i++)
+            map.put(list1[i], i);
+        List < String > res = new ArrayList < > ();
+        int min_sum = Integer.MAX_VALUE, sum;
+        for (int j = 0; j < list2.length && j <= min_sum; j++) {
+            if (map.containsKey(list2[j])) {
+                sum = j + map.get(list2[j]);
+                if (sum < min_sum) {
+                    res.clear();
+                    res.add(list2[j]);
+                    min_sum = sum;
+                } else if (sum == min_sum)
+                    res.add(list2[j]);
+            }
+        }
+        return res.toArray(new String[res.size()]);
+    }
+}
