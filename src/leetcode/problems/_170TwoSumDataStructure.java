6661c6f492ca8f9b09b31b58d3de8c8b2e6dc2cb
asari@udel.edu <asari@udel.edu>
2022-01-20 04:18:00 -0500

    170. Two Sum III - Data structure design

---
+++ src/leetcode/problems/_170TwoSumDataStructure.java
@@ -0,0 +1,62 @@
+package leetcode.problems;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/** 170. Two Sum III - Data structure design
+ * Design a data structure that accepts a stream of integers and checks if it has a pair of integers that sum up to a particular value.
+ *
+ * Implement the TwoSum class:
+ *     TwoSum() Initializes the TwoSum object, with an empty array initially.
+ *     void add(int number) Adds number to the data structure.
+ *     boolean find(int value) Returns true if there exists any pair of numbers whose sum is equal to value, otherwise, it returns false.
+ *
+ * Example 1:
+ * Input
+ * ["TwoSum", "add", "add", "add", "find", "find"]
+ * [[], [1], [3], [5], [4], [7]]
+ * Output
+ * [null, null, null, null, true, false]
+ *
+ * Explanation
+ * TwoSum twoSum = new TwoSum();
+ * twoSum.add(1);   // [] --> [1]
+ * twoSum.add(3);   // [1] --> [1,3]
+ * twoSum.add(5);   // [1,3] --> [1,3,5]
+ * twoSum.find(4);  // 1 + 3 = 4, return true
+ * twoSum.find(7);  // No two integers sum up to 7, return false
+ */
+public class _170TwoSumDataStructure {
+}
+
+class TwoSum {
+    private HashMap<Integer, Integer> num_counts;
+
+    /** Initialize your data structure here. */
+    public TwoSum() {
+        this.num_counts = new HashMap<Integer, Integer>();
+    }
+
+    /** Add the number to an internal data structure.. */
+    public void add(int number) {
+        if (this.num_counts.containsKey(number))
+            this.num_counts.replace(number, this.num_counts.get(number) + 1);
+        else
+            this.num_counts.put(number, 1);
+    }
+
+    /** Find if there exists any pair of numbers which sum is equal to the value. */
+    public boolean find(int value) {
+        for (Map.Entry<Integer, Integer> entry : this.num_counts.entrySet()) {
+            int complement = value - entry.getKey();
+            if (complement != entry.getKey()) {
+                if (this.num_counts.containsKey(complement))
+                    return true;
+            } else {
+                if (entry.getValue() > 1)
+                    return true;
+            }
+        }
+        return false;
+    }
+}
