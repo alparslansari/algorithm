61855086dff5b24c0c0cc4db1d387f375ffa7f7f
asari@udel.edu <asari@udel.edu>
2022-01-23 06:25:42 -0500

    Stack implementation

---
+++ src/leetcode/datastructures/stack/MyStack.java
@@ -0,0 +1,30 @@
+package leetcode.datastructures.stack;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyStack {
+    private List<Integer> data;
+    public MyStack() {
+        data = new ArrayList<>();
+    }
+    /** Insert an element into the stack */
+    public void push(int x) {
+        data.add(x);
+    }
+    /** Checks whether the queue is empty or not */
+    public boolean isEmpty(){
+        return data.isEmpty();
+    }
+    /** Get the top item from the queue. */
+    public int top(){
+        return data.get(data.size()-1);
+    }
+    /** Delete an element from the queue. Return true if the operation is successful. */
+    public boolean pop(){
+        if(isEmpty()) return false;
+        data.remove(data.size()-1);
+        return true;
+    }
+
+}
---
+++ src/leetcode/datastructures/stack/MyStackRunner.java
@@ -0,0 +1,14 @@
+package leetcode.datastructures.stack;
+
+public class MyStackRunner {
+    public static void main(String[] args) {
+        MyStack s = new MyStack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        for(int i=0;i<4;i++){
+            if(!s.isEmpty()) System.out.println(s.top());
+            System.out.println(s.pop());
+        }
+    }
+}
