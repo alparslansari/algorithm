3a6c1828f4a531d0d022424136e8e9d587091ee3
asari@udel.edu <asari@udel.edu>
2022-01-23 16:36:44 -0500

    recursive example

---
+++ src/leetcode/recursion/ReverseExample.java
@@ -0,0 +1,19 @@
+package leetcode.recursion;
+
+public class ReverseExample {
+    public void reverseString(char[] s) {
+        helper(0, s.length - 1, s);
+    }
+
+    private void helper(int start, int end, char [] s) {
+        if (start >= end) {
+            return;
+        }
+        // swap between the first and the last elements.
+        char tmp = s[start];
+        s[start] = s[end];
+        s[end] = tmp;
+
+        helper(start + 1, end - 1, s);
+    }
+}
