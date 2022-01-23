dad1651eccbc96c24a5b0388371e2da590d77e28
asari@udel.edu <asari@udel.edu>
2022-01-23 06:15:08 -0500

    279. Perfect Squares

---
+++ src/leetcode/problems/_279PerfectSquares.java
@@ -0,0 +1,108 @@
+package leetcode.problems;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/** 279. Perfect Squares
+ * Given an integer n, return the least number of perfect square numbers that sum to n.
+ * A perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself. For example, 1, 4, 9, and 16 are perfect squares while 3 and 11 are not.
+ *
+ * Example 1:
+ * Input: n = 12
+ * Output: 3
+ * Explanation: 12 = 4 + 4 + 4.
+ *
+ * Example 2:
+ * Input: n = 13
+ * Output: 2
+ * Explanation: 13 = 4 + 9.
+ *
+ * Constraints:
+ *     1 <= n <= 10^4
+ */
+public class _279PerfectSquares {
+
+    public int numSquaresDP(int n) {
+        int dp[] = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        // bottom case
+        dp[0] = 0;
+
+        // pre-calculate the square numbers.
+        int max_square_index = (int) Math.sqrt(n) + 1;
+        int square_nums[] = new int[max_square_index];
+        for (int i = 1; i < max_square_index; ++i)
+            square_nums[i] = i * i;
+
+        for (int i = 1; i <= n; ++i)
+            for (int s = 1; s < max_square_index; ++s) {
+                if (i < square_nums[s])
+                    break;
+                dp[i] = Math.min(dp[i], dp[i - square_nums[s]] + 1);
+            }
+        return dp[n];
+    }
+
+
+    /**
+     * Math Base solution
+     */
+    protected boolean isSquare(int n) {
+        int sq = (int) Math.sqrt(n);
+        return n == sq * sq;
+    }
+
+    public int numSquaresMathBased(int n) {
+        // four-square and three-square theorems.
+        while (n % 4 == 0)
+            n /= 4;
+        if (n % 8 == 7)
+            return 4;
+
+        if (this.isSquare(n))
+            return 1;
+        // enumeration to check if the number can be decomposed into sum of two squares.
+        for (int i = 1; i * i <= n; ++i) {
+            if (this.isSquare(n - i * i))
+                return 2;
+        }
+        // bottom case of three-square theorem.
+        return 3;
+    }
+
+    /**
+     * Approach 4: Greedy + BFS (Breadth-First Search)
+     */
+    public int numSquares(int n) {
+        ArrayList<Integer> square_nums = new ArrayList<Integer>();
+        for (int i = 1; i * i <= n; ++i) {
+            square_nums.add(i * i);
+        }
+
+        Set<Integer> queue = new HashSet<Integer>();
+        queue.add(n);
+
+        int level = 0;
+        while (queue.size() > 0) {
+            level += 1;
+            Set<Integer> next_queue = new HashSet<Integer>();
+
+            for (Integer remainder : queue) {
+                for (Integer square : square_nums) {
+                    if (remainder.equals(square)) {
+                        return level;
+                    } else if (remainder < square) {
+                        break;
+                    } else {
+                        next_queue.add(remainder - square);
+                    }
+                }
+            }
+            queue = next_queue;
+        }
+        return level;
+    }
+
+}
