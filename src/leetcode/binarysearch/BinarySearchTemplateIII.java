518f2bb20d27778f6f94283adb4a7874befc61fa
asari@udel.edu <asari@udel.edu>
2022-01-26 04:54:58 -0500

    Binary Search Template III

--- src/leetcode/binarysearch/BinarySearchTemplateIII.java
+++ src/leetcode/binarysearch/BinarySearchTemplateIII.java
@@ -1,5 +1,24 @@
 package leetcode.binarysearch;
 
+/**
+ * Template #3 is another unique form of Binary Search. It is used to search for an element or
+ * condition which requires accessing the current index and its immediate left and right
+ * neighbor's index in the array.
+ *
+ * Key Attributes:
+ *     An alternative way to implement Binary Search
+ *     Search Condition needs to access element's immediate left and right neighbors
+ *     Use element's neighbors to determine if condition is met and decide whether to go left or right
+ *     Gurantees Search Space is at least 3 in size at each step
+ *     Post-processing required. Loop/Recursion ends when you have 2 elements left.
+ *     Need to assess if the remaining elements meet the condition.
+ *
+ * Distinguishing Syntax:
+ *     Initial Condition: left = 0, right = length-1
+ *     Termination: left + 1 == right
+ *     Searching Left: right = mid
+ *     Searching Right: left = mid
+ */
 public class BinarySearchTemplateIII {
     int binarySearch(int[] nums, int target) {
         if (nums == null || nums.length == 0) return -1;
