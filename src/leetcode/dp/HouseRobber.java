0fd5273ea287f5a10cdf6b1cb42c5e4170359c70
asari@udel.edu <asari@udel.edu>
2022-01-29 06:14:44 -0500

    Dynamic programming

--- src/leetcode/dp/HouseRobber.java
+++ src/leetcode/dp/HouseRobber.java
@@ -1,2 +1,24 @@
-package leetcode.dp;public class HouseRobber {
+package leetcode.dp;
+
+import java.util.HashMap;
+
+public class HouseRobber {
+    private HashMap<Integer, Integer> memo = new HashMap<>();
+    private int[] nums;
+
+    private int dp(int i) {
+        // Base cases
+        if(i == 0) return nums[0];
+        if(i == 1) return Math.max(nums[0], nums[1]);
+        if(!memo.containsKey(i)){
+            // Recurrence relation
+            memo.put(i, Math.max(dp(i-1), dp(i-2)+nums[i]));
+        }
+        return memo.get(i);
+    }
+
+    public int rob(int[] nums) {
+        this.nums = nums;
+        return dp(nums.length-1);
+    }
 }
