8ef8e26dcb023155c0ca250ab957c910506e0d28
Alparslan Sari <asari@udel.edu>
2022-02-12 15:13:13 -0500

    50 pow

---
+++ src/leetcode/problems/linkedin/_50Pow.java
@@ -0,0 +1,60 @@
+package leetcode.problems.linkedin;
+
+/** 50. Pow(x, n)
+ * Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
+ *
+ * Example 1:
+ * Input: x = 2.00000, n = 10
+ * Output: 1024.00000
+ *
+ * Example 2:
+ * Input: x = 2.10000, n = 3
+ * Output: 9.26100
+ *
+ * Example 3:
+ * Input: x = 2.00000, n = -2
+ * Output: 0.25000
+ * Explanation: 2-2 = 1/22 = 1/4 = 0.25
+ */
+public class _50Pow {
+
+    /** Fast Power Algorithm Iterative: Using the formula x ^ {a + b} = x ^ a * x ^ b
+     * Complexity Analysis
+     *     Time complexity : O(logn). For each bit of n's binary representation,
+     *     we will at most multiply once. So the total time complexity is O(logn).
+     *
+     *     Space complexity : O(1). We only need two variables for the current
+     *     product and the final result of x.
+     */
+    public double myPow(double x, int n) {
+        long N = n;
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        double ans = 1;
+        double current_product = x;
+        for (long i = N; i > 0; i /= 2) {
+            if ((i % 2) == 1) {
+                ans = ans * current_product;
+            }
+            current_product = current_product * current_product;
+        }
+        return ans;
+    }
+
+    /** Approach: Brute Force
+     * Time Limit Exceeded
+     */
+    public double myPowBruteForce(double x, int n) {
+        long N = n;
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        double ans = 1;
+        for (long i = 0; i < N; i++)
+            ans = ans * x;
+        return ans;
+    }
+}
