86c43dd09690687b230033d6c85d16d9f168f735
Alparslan Sari <asari@udel.edu>
2022-02-24 10:52:04 -0500

    139, 319, 464

--- src/leetcode/problems/linkedin/_319BulbSwitcher.java
+++ src/leetcode/problems/linkedin/_319BulbSwitcher.java
@@ -1,5 +1,7 @@
 package leetcode.problems.linkedin;
 
+import java.util.Arrays;
+
 /** 319. Bulb Switcher
  * There are n bulbs that are initially off. You first turn on all the bulbs, then you turn off every second bulb.
  * On the third round, you toggle every third bulb (turning on if it's off or turning off if it's on).
@@ -27,5 +29,23 @@ package leetcode.problems.linkedin;
 public class _319BulbSwitcher {
     public int bulbSwitch(int n) {
         return (int)Math.sqrt(n);
+    }
+
+    public int bulbSwitchBF(int n) {
+        if(n==1) return 1;
+        int[] bulbs = new int[n+1];
+        Arrays.fill(bulbs, 1); // first round
+        for(int i=2;i<n;i++)
+            for(int j=i;j<n+1;j=j+i)
+            {
+                if(j>n) break;
+                bulbs[j] = bulbs[j] == 0 ? 1 : 0;
+            }
+        bulbs[n] = bulbs[n] == 0 ? 1 : 0;
+        int result = 0;
+        for(int i=1;i<n+1;i++)
+            result += bulbs[i] == 1 ? 1 : 0;
+
+        return result;
     }
 }
