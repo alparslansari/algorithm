eb411afe2ef7284307163e10b0ba427b13f86e78
asari@udel.edu <asari@udel.edu>
2022-02-24 05:20:56 -0500

    ListNode

--- src/leetcode/models/ListNode.java
+++ src/leetcode/models/ListNode.java
@@ -3,8 +3,13 @@ package leetcode.models;
 public class ListNode {
     public int val;
     public ListNode next;
-    ListNode(int x) {
+    public ListNode(int x) {
         val = x;
         next = null;
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
     }
 }
